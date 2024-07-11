56e83775ff921f0f03d5802fa67a590bc6804c67
zenifa317 <zenifa2001317@gmail.com>
2024-07-11 22:56:33 +0900

    shファイル：せん断追加

--- ImgAugmentTool.sh
+++ ImgAugmentTool.sh
@@ -1,5 +1,8 @@
 #画像回転
-#python3 ImgAugmentTool.py --path '' --new-path '' --process 'rotate' --angle
+#python3 ImgAugmentTool.py --input '' --output '' --process 'rotate' --angle
 
 #反転
-#python3 ImgAugmentTool.py --path '' --new-path '' process 'flip' --flipecode
+#python3 ImgAugmentTool.py --input '' --output '' --process 'flip' --flipecode
+
+#せん断
+#python3 ImgAugmentTool.py --input '' --output '' --process 'shear' --shear_factor   --shear_point  

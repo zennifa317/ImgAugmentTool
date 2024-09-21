9de9d533c570d8f4add2e2679d98ba073579b797
zenifa317 <zenifa2001317@gmail.com>
2024-09-21 19:44:53 +0900

    修正

--- yolo_api.py
+++ yolo_api.py
@@ -1,4 +1,5 @@
 import cv2
+from collections import defaultdict
 import os
 
 class yolo_format:
@@ -25,26 +26,43 @@ class yolo_format:
 
         if annotations_file is not None:
             print('loading annotations file...')
-            anns = {}
+            anns = defaultdict(list)
             for ann_path in annotations_file:
                 img_id = os.path.splitext(os.path.basename(ann_path))[0]
-                ann_info = {}
                 with open(ann_path, 'r') as f:
                     ann = f.readlines()
                 for info in ann:
+                    ann_info = {}
+
                     cat_id, bbox = info.split(' ', 1)
 
                     ann_info['cat_id'] = cat_id
                     ann_info['bbox'] = bbox
 
-                anns[img_id] = ann_info
+                    anns[img_id].append(ann_info)
             
             self.anns = anns
             print('Done')
 
     def save(self, img_id, output):
+        image_path = os.path.join(output, 'images')
+        label_path = os.path.join(output, 'labels')
+
         os.makedirs(output, exist_ok=True)
+        if not os.path.exists(image_path):
+            os.mkdir(image_path)
+        if not os.path.exists(label_path):
+            os.mkdir(label_path)
 
         img_info = self.imgs[img_id]
         img = cv2.imread(img_info['path'])
-        cv2.imwrite(os.path.join(output, img_info['img_name']), img)
+        cv2.imwrite(os.path.join(image_path, img_info['img_name']), img)
+
+        anns_info = self.anns[img_id]
+        anns = []
+        for ann_info in anns_info:
+            ann = ann_info['cat_id'] + ' ' + ann_info['bbox']
+            anns.append(ann)
+        print(anns)
+        with open(os.path.join(label_path, img_id+'.txt'), mode='w') as f:
+            f.write(''.join(anns))
